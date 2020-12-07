568cc4ff64a25269311b6085fa4facea065de3db
Tanguy Gérôme <tanguy.gerome@gmail.com>
2020-12-07 22:26:28 +0100

    base Navigation Drawer example

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 #!/bin/env python3
-from kivy.uix.screenmanager import Screen
 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
+
+
+class NavigationDrawerContents(BoxLayout):
+    pass
+
+
+class MainScreen(Screen):
+    pass
 
 
 class MainApp(MDApp):
     def build(self):
-        screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Hello, World",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
-        )
-        return screen
+        return MainScreen()
 
 
 MainApp().run()
